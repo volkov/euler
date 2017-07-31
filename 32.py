e357bd218c9cbbb8a5240a7598c5630733734869
Sergey A Volkov <serg-v@yandex-team.ru>
2017-08-01 00:04:36 +0300

    problems 31 32

---
+++ 32.py
@@ -0,0 +1,14 @@
+pp = set()
+for i in range(1,5001):
+    for j in range(i,5001):
+        p = i * j
+        li = list(str(i))
+        lj = list(str(j))
+        lp = list(str(p))
+        if len(li)+len(lj)+len(lp)==9:
+            s = set(li+lj+lp)
+            if len(s)==9 and '0' not in s:
+                print("{}*{}={}".format(i,j,p))
+                pp.add(p)
+print(sum(pp))
+
