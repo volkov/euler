cd5d10c459f556eea14b7f330f03223298b459e1
Sergey A Volkov <serg-v@yandex-team.ru>
2017-08-28 00:28:17 +0300

    78

--- 78.py
+++ 78.py
@@ -1,16 +1,19 @@
-L = 30000 
-M = [0]*(L+1)
-M0 = [0]*(L+1)
+L = 100000
+M = [0] * (L + 1)
+M0 = [0] * (L + 1)
 M0[0] = 1
-for i in range(1, L+1):
-    for j in range(0, L+1):
-        for k in range(j, -1, -i):
-            M[j] += M0[k]
-    if i%100==0:
-        print("Processed",i)
-    #print(M)
-    if M[i]%1000000==0:
-        print(i,M[i])
+for i in range(1, L + 1):
+    for j in range(0, L + 1):
+        M[j] += M0[j]
+        if j - i >= 0:
+            M[j] += M[j - i]
+    if i % 100 == 0:
+        print("Processed", i)
+    print(M)
+    if M[i] % 100000 == 0:
+        print(i, M[i])
+    if M[i] % 1000000 == 0:
+        print("Answer",i, M[i])
         quit()
-    M0=M
-    M=[0]*(L+1)
+    M0 = M
+    M = [0] * (L + 1)
